d620e397de32dbdc0c8697750eb69e8586044eb1
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-06-05 16:53:32 +0500

    2 задание Усложненное

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
-let num  = 266219;
-let mult = 1;
+let num  = 266219,
+mult = 1,
+ost;
 
 for (let i = num; Math.trunc(i) > 0; i /= 10) {
     mult *= Math.trunc(i) % 10;
@@ -8,7 +9,6 @@ for (let i = num; Math.trunc(i) > 0; i /= 10) {
 console.log('Произведение :', mult);
 mult **= 3;
 console.log('Степень :', mult);
-let ost;
 
 for (ost = mult ; ost > 100; ost/=10) {}
 
