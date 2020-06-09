98e5bb19db41caf208ae177b7d19f48269a6ec2b
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-06-09 17:42:11 +0500

    4 урок

--- script/script.js
+++ script/script.js
@@ -1,51 +1,24 @@
 'use strict';
 
-let leng,
-ruDays = 'Понедельник, вторник, среда....',
-enDays = 'Sunday, Monday, Tuesday.....';
+let nextD = ' 12312312312312312312312312312312312312312312312312 ';
 
-while (leng !== 'ru' && leng !== 'en') {
-    leng = prompt('ru or en');
-}
+let hiFromGordonRamz = function(next) {
+    if(typeof next == 'number') {
+        return('Принимаются только строки!!!!');
+    }
 
-if (leng === 'ru') {
-    console.log(ruDays);
-} else {
-    console.log(enDays);
-}
+    next = next.trim();
 
-switch(leng) {
-    case 'ru':
-        console.log(ruDays);
-        break;
-    case 'en':
-        console.log(enDays);
-        break;
-}
+    if (next.length > 30) {                 
+        next = next.slice(0, 30).split('');
+        for(let i = 30; i<33; i++) {
+            next[i] = '.';
+        }
+    }
+    
+    next = next.join('');
 
-let arrDoble = [   
-    ['ru'],
-    ['en']
-];
-arrDoble['ru'] = ruDays;
-arrDoble['en'] = enDays;
-console.log(arrDoble[leng]);
-
-let arrDobleTwo = [];
-arrDobleTwo['ru'] = ['пн', 'вт', 'ср'];
-arrDobleTwo['en'] = ['mon', 'tue', 'thu'];
-console.log(arrDobleTwo[leng]);
-
-let obj = {
-    'ru': ruDays,
-    'en': enDays
+    return next;
 };
 
-console.log(obj[leng]);
-
-let namePerson;
-namePerson = prompt('Введите имя');
-
-namePerson === 'Артем' ? console.log('Директор') : 0;
-namePerson === 'Максим' ? console.log('Преподаватель') : 0;
-namePerson !==  'Максим' || 'Артем' ? console.log('Студент') : 0;
+console.log(hiFromGordonRamz(nextD));
