e7a5ead933eb3b25d2dd5c37e1134c03e1fb1dcc
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-06-11 16:37:40 +0500

    6 урок

--- script/script.js
+++ script/script.js
@@ -11,7 +11,7 @@ function randomGame() {
     let randomNumber = Math.floor(Math.random() * (100 - 1)) + 1,
     userNumber = prompt('Угадайте число от 1 до 100'),
     tryCount = 10;
-    alert(randomNumber);
+
     function game() {
         console.dir(game);
         
