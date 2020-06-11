1357f1e7d1b659e1bf150c36dd34f85fcd75d009
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-06-11 16:36:02 +0500

    6 урок

--- script/script.js
+++ script/script.js
@@ -1,22 +1,79 @@
 'use strict';
 
-let arr = ['123', '2666', '4555', '7567', '5435', '1111', '46456'];
 
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i].startsWith("4") || arr[i].startsWith("2")){
-        console.log(arr[i]);
-    }
+let isNumber = function(n) {
+    return !isNaN(parseInt(n)) && isFinite(n);
 }
 
-for (let i = 2; i <= 100; i++) { 
-    let bool = true;
-    for(let j = 2; j < i; j++) {
-        if (i % j === 0) {
-            bool = false;
-            break;
+
+
+function randomGame() {
+    let randomNumber = Math.floor(Math.random() * (100 - 1)) + 1,
+    userNumber = prompt('Угадайте число от 1 до 100'),
+    tryCount = 10;
+    alert(randomNumber);
+    function game() {
+        console.dir(game);
+        
+        if (userNumber === null) {
+            alert('тут игра должна закончиться');
+            return;
+        }
+
+        if (!isNumber(userNumber)) {
+            alert('Вы ввели не число');
+            userNumber = prompt('Введите новый вариант');
+            return game();
+        }
+
+        if (userNumber > randomNumber) {
+            tryCount -= 1;
+            alert('Загаданное число меньше, осталось попыток ' + tryCount);
+            if (tryCount === 0) {
+                return userChoiceFail();
+            }
+            userNumber = prompt('Введите новый вариант');
+            return game();
+        }
+        
+        if (userNumber < randomNumber) {
+            tryCount -= 1;
+            alert('Загаданное число больше, осталось попыток ' + tryCount);
+            if (tryCount === 0) {
+                return userChoiceFail();
+            }
+            userNumber = prompt('Введите новый вариант');
+            return game();
+        }
+
+        if (userNumber == randomNumber) {
+            return userChoiceWin();
         }
     }
-    if (bool == true) {
-        console.log(i);
+    game();
+
+    function userChoiceFail(count) {
+        let choice = confirm('Попытки закончились, хотите сыграть еще?');
+        if(choice) {
+            tryCount = 10;
+            userNumber = 0;
+            randomGame();
+        } else {
+            return alert('тут игра должна закончиться');;
+        }
     }
+
+    function userChoiceWin(count) {
+        let choice = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+        if(choice) {
+            tryCount = 10;
+            userNumber = 0;
+            randomGame();
+        } else {
+            return alert('тут игра должна закончиться');;
+        }
+    }
+
 }
+
+randomGame();
