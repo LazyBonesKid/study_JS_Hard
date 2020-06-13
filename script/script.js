f042b869e8bada9a5017b396028a1caf537f2f91
Дамир Саттаров <61625025+LazyBonesKid@users.noreply.github.com>
2020-06-14 00:43:29 +0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,17 +2,21 @@
 
 let Data = new Date().getDay();
 
-let arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let arr = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', ];
 
-for(let i = 0; i < arr.length; i++) {
-    
+for(let i = 1; i < arr.length; i++) {
     if (i == Data){
         document.write(`<p><i>${arr[i]}</i></p>`);
-    } else if (arr[i] == 'Суббота' || arr[i] == 'Воскресенье') {
+    } else if (arr[i] == 'Суббота') {
         document.writeln(`<p><b>${arr[i]}</b></p>`);
     } else {
         document.write(`<p>${arr[i]}</p>`);
     }
-
+    if (i == 6) {
+        if(Data == 0) {
+            document.write(`<p><i>${arr[0]}</i></p>`);
+        } else {
+            document.writeln(`<p><b>${arr[0]}</b></p>`);
+        }
+    } 
 }
-
