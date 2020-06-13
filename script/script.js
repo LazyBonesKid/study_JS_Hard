138b1bf338ffe5615c9adcabc30f16912673d65f
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-06-13 21:10:54 +0500

    7 урок

--- script/script.js
+++ script/script.js
@@ -1,79 +1,20 @@
 'use strict';
 
+let Data = new Date().getDay() - 1;
 
-let isNumber = function(n) {
-    return !isNaN(parseInt(n)) && isFinite(n);
-}
+console.log(Data);
 
+let arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
-
-function randomGame() {
-    let randomNumber = Math.floor(Math.random() * (100 - 1)) + 1,
-    userNumber = prompt('Угадайте число от 1 до 100'),
-    tryCount = 10;
-
-    function game() {
-        console.dir(game);
-        
-        if (userNumber === null) {
-            alert('тут игра должна закончиться');
-            return;
-        }
-
-        if (!isNumber(userNumber)) {
-            alert('Вы ввели не число');
-            userNumber = prompt('Введите новый вариант');
-            return game();
-        }
-
-        if (userNumber > randomNumber) {
-            tryCount -= 1;
-            alert('Загаданное число меньше, осталось попыток ' + tryCount);
-            if (tryCount === 0) {
-                return userChoiceFail();
-            }
-            userNumber = prompt('Введите новый вариант');
-            return game();
-        }
-        
-        if (userNumber < randomNumber) {
-            tryCount -= 1;
-            alert('Загаданное число больше, осталось попыток ' + tryCount);
-            if (tryCount === 0) {
-                return userChoiceFail();
-            }
-            userNumber = prompt('Введите новый вариант');
-            return game();
-        }
-
-        if (userNumber == randomNumber) {
-            return userChoiceWin();
-        }
-    }
-    game();
-
-    function userChoiceFail(count) {
-        let choice = confirm('Попытки закончились, хотите сыграть еще?');
-        if(choice) {
-            tryCount = 10;
-            userNumber = 0;
-            randomGame();
-        } else {
-            return alert('тут игра должна закончиться');;
-        }
-    }
-
-    function userChoiceWin(count) {
-        let choice = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-        if(choice) {
-            tryCount = 10;
-            userNumber = 0;
-            randomGame();
-        } else {
-            return alert('тут игра должна закончиться');;
-        }
+for(let i = 0; i < arr.length; i++) {
+    
+    if (i == Data){
+        document.write(`<p><i>${arr[i]}</i></p>`);
+    } else if (arr[i] == 'Суббота' || arr[i] == 'Воскресенье') {
+        document.writeln(`<p><b>${arr[i]}</b></p>`);
+    } else {
+        document.write(`<p>${arr[i]}</p>`);
     }
 
 }
 
-randomGame();
