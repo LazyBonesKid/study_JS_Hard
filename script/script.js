3d3209c6b293cdfd163f2cd14630f099fe105af2
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-06-16 19:21:16 +0500

    9 урок

--- script/script.js
+++ script/script.js
@@ -1,139 +1,61 @@
-'use strict';
-
-let isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-let isString = function (n) {
-    let re = /[0-9]/g;
-    return re.test(n) || n === null || !n.trim()
-}
-
-let start = function () {
-    do {
-        start = prompt ('Ваш месячный доход?' , 30000);
-    } while (!isNumber(start));
-    return start;
-};
-
 let
-money = start();
+    date    = new Date(),
 
-let appData = {
-    income: {},
-    expenses: {},
-    addIncome: [],
-    addExpenses: [],
-    deposit: false,
-    mission: 100500,
-    period: 11,
-    budget: money,
-    percentDeposit: 0,
-    percentMoney: 0,
-    budgetDay: 0,
-    budgetMonth: 0,
-    expensesMonth: 0,
+    dayArr      = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+    monthArr    = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
 
-    asking: function() {
-        
-        if(confirm('Есть ли у вас дополнительный заработок?')) {
-            let  itmeIncome, 
-            cashIncome;
+    arrJas  = [1, 21],
+    arrJasa = [2, 3, 4, 22, 23, 24];
 
-            do {
-                itmeIncome = prompt('Какой у вас дополнительный заработок?', 'Таксую');
-            } while (isString(itmeIncome));
-            do {
-                cashIncome = prompt('сколько в месяц вы на этом зарабатываете?', 15000);
-            } while (!isNumber(cashIncome));
-            
-            appData.income[itmeIncome] = cashIncome;
-        }
-
-        let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую' , 'интеРнет, таКси, комМунальные расходы');
-        appData.addExpenses = addExpenses.toLocaleLowerCase().split(', ');
-        appData.deposit = confirm('Есть ли у вас депозит в банке?');
-        
-        for (let i = 0; i < 2; i++) {
-            let key,
-            sum;
-
-            do {
-                key = prompt('Введите обязательную статью расходов', 'водичка');
-            } while (isString(key));
-
-            do {
-                sum = prompt('Во сколько это обойдется?', 450);
-            } while (!isNumber(sum));
-
-            appData.expenses[key] = +sum;
-        }
-        
-        for (let key in appData.expenses) {
-            appData.expensesMonth += appData.expenses[key];
-        }
-    },
-
-
-    getBudget: function () {  
-        appData.budgetMonth = appData.budget - appData.expensesMonth;
-        appData.budgetDay = Math.floor(appData.budgetMonth / 30);
-    },
-
-    getTargetMonth: function () {
-    
-        if (appData.budgetDay > 0){
-            return 'Цель будет достигнута через: ' + Math.ceil(appData.mission/appData.budgetMonth) + ' месяца';
-        } else {
-            return 'Цель не будет достигнута';
-        }
-    },
-
-    getStatusIncome: function() {
-        if (appData.budgetDay >= 1200) {
-            return('У вас высокий уровень дохода');
-        } else if (appData.budgetDay >= 600 && appData.budgetDay < 1200) {
-            return('У вас средний уровень дохода');
-        } else if (appData.budgetDay < 600 && appData.budgetDay >= 0) {
-            return('К сожалению у вас уровень дохода ниже среднего');
-        } else if (appData.budgetDay < 0 ) {
-            return('Что то пошло не так');
-        } 
-    },
-
-    getInfoDeposit: function () {
-        if(appData.deposit) {
-            do {
-                appData.percentDeposit = prompt('Какой годовой процент?', 10);
-            } while (!isNumber(appData.percentDeposit));
-            do {
-                appData.moneyDeposit = prompt('какая сумма заложена?', 10000);
-            } while (!isNumber(appData.moneyDeposit));
-        }
-    },
-
-    calcSavedMoney: function () {
-        return appData.budgetMonth * appData.period;
+    function checkA (arr) {
+        return arr.some(function (element) {
+            return element == date.getHours;
+        });
     }
-};
+    
+function A () {
+    if (checkA(arrJas)) {
+        document.write('а) ' + 'Cегодня ' + dayArr[date.getDay()] + ', ' + date.getDate() + ' ' + monthArr[date.getMonth()] + ' ' + date.getFullYear() + ' года, ' + 
+        date.getHours() + ' час ' + date.getMinutes() + ' минут ' + date.getSeconds() + ' cекунды' + '<br/>' + '<br/>');
+    } else if (checkA(arrJasa)) {
+        document.write('а) ' + 'Cегодня ' + dayArr[date.getDay()] + ', ' + date.getDate() + ' ' + monthArr[date.getMonth()] + ' ' + date.getFullYear() + ' года, ' + 
+        date.getHours() + ' часа ' + date.getMinutes() + ' минут ' + date.getSeconds() + ' cекунды' + '<br/>' + '<br/>');
+    } else {
+        document.write('а) ' + 'Cегодня ' + dayArr[date.getDay()] + ', ' + date.getDate() + ' ' + monthArr[date.getMonth()] + ' ' + date.getFullYear() + ' года, ' + 
+        date.getHours() + ' часов ' + date.getMinutes() + ' минут ' + date.getSeconds() + ' cекунды' + '<br/>' + '<br/>');
+    }
+}
 
-appData.asking();
-appData.getBudget();
-console.log('Расходы за месяц:', appData.expensesMonth);
-console.log('Период равен ' + appData.period + ' месяцев');
-console.log('Цель заработать ' + appData.mission + ' крышек нюка колы');
-console.log(appData.getTargetMonth());
-console.log(appData.getStatusIncome());
 
-/* console.log('Наша программа включает в себя данные: ');
-for (let key in appData) {
-    console.log('Свойство ' + key + ' Значение ' + appData[key]);
-} */
+A();
 
-appData.getInfoDeposit();
+/////////////////////////////////////////////////////////
 
-console.log(appData.addExpenses.map(function(item, i , arr1) {
-    return `${item[0].toUpperCase()}${item.slice(1)}`;
-}).join(', '));
+function checkB (number) {
+    if (number < 10) {
+        return '0' + number;
+    }
+    return number;
+}
 
-console.log(appData.calcSavedMoney(), appData.percentDeposit, appData.moneyDeposit);
+function B () {
+
+
+    document.write('б) ' + checkB(date.getHours()) + ':' + checkB(date.getMinutes()) + ':' + checkB(date.getSeconds()) + ' ' + 
+    checkB(date.getDate()) + '.' + checkB(date.getMonth()+1) + '.' + date.getFullYear() + '<br/>' + '<br/>');
+
+}
+
+B();
+
+/////////////////////////////////////////////////////////
+let div = document.createElement('div');
+document.body.append(div);
+
+function C () {
+    let date = new Date();
+    document.querySelector('div').innerHTML = ('c) ' + checkB(date.getHours()) + ':' + checkB(date.getMinutes()) + ':' + checkB(date.getSeconds()) + ' ' + 
+    checkB(date.getDate()) + '.' + checkB(date.getMonth()+1) + '.' + date.getFullYear() + '<br/>' + '<br/>');
+}
+
+setInterval(C,1000);
